6b7fd95e9f4afd366186717deacdf218ff9be4d4
anvith <anand.anvith@gmail.com>
2024-03-06 18:36:47 -0500

    fix py script

--- quickstart.py
+++ quickstart.py
@@ -4,33 +4,30 @@
 
 from datetime import datetime
 
-from openai import OpenAI
 import os
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+from openai import OpenAI
 
+client = OpenAI(
+    api_key="Secret_key" # remember to use key
+)
 
 def get_curr_details():
   now = datetime.now()
   return now.strftime("%H:%M:%S %Y-%m-%d %A")
 
-mic_record = "Lets meet next tuesday at noon instead"
+mic_record = "Lets meet on july 12th at 8pm"
 
-query = "The date & time now is", get_curr_details(), "and the transcripted voice recording is: ", mic_record,
-"Based on the current date & time as well as the transcripted voice recording, calculate and return the date and time that the event has to be scheduled on",
-"The output should be in the format YYYY-MM-DD-HH-MM-SS. Do not return anything else."
+query = "The date & time now is"+ get_curr_details() + \
+    "and the transcripted voice recording is: "+ mic_record + \
+    "Based on the current date & time as well as the transcripted voice recording, calculate and return the date and time that the event has to be scheduled on" \
+    "The output should be in the format YYYY-MM-DD-HH-MM-SS. Do not return anything else."
 
-response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": "you are a time retrieval assistant"
-        },
-        {"role": "user", 
-         "content": query
-        }
-    ]
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": query}
+  ]
 )
-print(response)
-  
+
+print(completion.choices[0].message.content)
